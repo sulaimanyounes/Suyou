03cabf6e6ba2818e3947fce6e92f27120683d8d0
sulaimanyounes <sulaiman.younes-ext@viavarejo.com.br>
2023-10-14 22:22:52 -0300

    Logs Implementation

--- Suyou/src/Suyou.h
+++ Suyou/src/Suyou.h
@@ -2,7 +2,10 @@
 
 // For use by Suyou applications
 #include "Suyou/Application.h"
+#include "Suyou/Log.h"
 
 // ----------Entry Point-----------
 #include "Suyou/EntryPoint.h"
 //---------------------------------
+
+extern Suyou::Application* Suyou::CreateApplication();
--- Suyou/src/Suyou/EntryPoint.h
+++ Suyou/src/Suyou/EntryPoint.h
@@ -2,10 +2,16 @@
 
 #ifdef SY_PLATFORM_WINDOWS
 
-extern Suyou::Application* Suyou::CreateApplication();
+//extern Suyou::Application* Suyou::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Suyou::Log::Init();
+	SY_CORE_WARN("Initialized Log");
+
+	int a = 5;
+	SY_TRACE("Hello! Var={0}", a);
+
 	auto app = Suyou::CreateApplication();
 	app->Run();
 	delete app;
