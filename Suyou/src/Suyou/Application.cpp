06fc5acaf465747b5b43d3fa71d852dbcf412324
sulaimanyounes <sulaiman.younes-ext@viavarejo.com.br>
2023-10-18 16:00:09 -0300

    Implementing glfw calkbacks

--- Suyou/src/Suyou/Application.cpp
+++ Suyou/src/Suyou/Application.cpp
@@ -1,20 +1,30 @@
 #include "sypch.h"
 #include "Application.h"
 
-#include "Suyou/Events/ApplicationEvent.h"
 #include "Suyou/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Suyou
 {
+	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SY_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,4 +36,11 @@ namespace Suyou
 			m_Window->OnUpdate();
 		}
 	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
 }
--- Suyou/src/Suyou/Application.h
+++ Suyou/src/Suyou/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Suyou/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Suyou
@@ -13,7 +15,11 @@ namespace Suyou
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
